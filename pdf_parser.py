731af4a4ae6d8814115fec61b006679925ed0242
LukasBall <lukas.ballweg@riseup.net>
2020-12-11 22:47:17 +0100

    first project structure

---
+++ pdf_parser.py
@@ -0,0 +1,8 @@
+def extract_from_pdf(file_path):
+    '''
+    returns text from a given PDF file
+    :param file: input-file
+    :return: text as string
+    '''
+    text = ""
+    return text
---
+++ vocab_extractor.py
@@ -0,0 +1,9 @@
+def extract_vocab(text):
+    '''
+    :param text: input text where vocabulary should be extracted from
+    :return: dictionary? including the complete vocab, part of speech information and frequency
+    '''
+
+    # create dictionary with vocab | part of speech | frequency
+    # use pandas like in assignment 2 Nr. 1?
+
