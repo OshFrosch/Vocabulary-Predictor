45ad03cef7e840ed72a7b811e0b2639ade5e2870
LukasBall <lukas.ballweg@riseup.net>
2020-12-11 22:57:30 +0100

    add extract-text-from-pdf functionality

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,8 +1,18 @@
-def extract_from_pdf(file_path):
-    '''
-    returns text from a given PDF file
+from pdfminer.high_level import extract_text
+
+
+def extract_from_file(filepath):
+    """
+    returns text from a given file (PDF or txt)
     :param file: input-file
     :return: text as string
-    '''
-    text = ""
+    """
+
+    # TODO: if file is PDF
+    if True:
+        text = extract_text(filepath)
+
+    # TODO: if file is txt, extract text from txt file
+    # TODO: add additional file types like images
+
     return text
