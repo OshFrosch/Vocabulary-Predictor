7f5169a88b08fae9037bfa43a9d5c58d935d5d6b
LukasBall <lukas.ballweg@riseup.net>
2021-01-21 01:06:12 +0100

    style improvements

--- src/pipeline/pipeline_components/pipe_example_phrase.py
+++ src/pipeline/pipeline_components/pipe_example_phrase.py
@@ -1,34 +1,37 @@
 def get_example_phrase(doc):
-    """providing an example sentence for every vocab"""
+    """
+    providing an example sentence from the doc for every vocab
+    :param doc: spaCy doc
+    :return: doc with added example_phrase attributes
+    """
 
     token_sentences = {}
-    for sent in doc.sents:
+    for sentence in doc.sents:
 
-        sentence_difficulty = calculate_sentence_ranking(sent)
+        sentence_difficulty = calculate_sentence_ranking(sentence)
 
-        for token in sent:
+        for token in sentence:
             if not token._.is_excluded:
-                token_sentences[token.text] = [(sent.text, sentence_difficulty)]
+                token_sentences[token.text] = [(sentence.text, sentence_difficulty)]
             elif token.text in token_sentences.keys():
-                token_sentences[token.text].append((sent.text, sentence_difficulty))
+                token_sentences[token.text].append((sentence.text, sentence_difficulty))
 
     for token in doc:
         if not token._.is_excluded:
             phrases = token_sentences[token.text]
             best_phrase = min(phrases, key=lambda item: (item[1]))[0]
             token._.example_phase = best_phrase
-
     return doc
 
 
-def calculate_sentence_ranking(sent):
+def calculate_sentence_ranking(sentence):
     """
     The ranking consits of the cumulated difficulty of non-excluded vocab words in the sentence
-    :param sent: sentence from spacy doc
+    :param sentence: sentence from spacy doc
     :return: sentence_difficulty
     """
     sentence_difficulty = 0
-    for token in sent:
+    for token in sentence:
         if not token._.is_excluded:
             sentence_difficulty += token._.difficulty
     return sentence_difficulty
