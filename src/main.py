75e942b06e937aebd6fe0793889fad9c732d4b76
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-02-01 01:05:19 +0100

    fixes document referencing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
-import os
-
-import spacy
 from pipeline.create_pipeline import create_pipeline
-from spacy.matcher import PhraseMatcher
 from utils.input_utils.text_extraction import extract_from_file
 from utils.output_utils.create_df_from_doc import create_df_from_doc
 
 if __name__ == "__main__":
-    print(os.getcwd())
     text = extract_from_file("src/data/kafka.txt")
 
     pipeline = create_pipeline()
