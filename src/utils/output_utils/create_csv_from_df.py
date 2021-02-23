f7008a7070b9dd21877e64fe8850fbe7821c545b
LukasBall <lukas.ballweg@riseup.net>
2021-02-23 20:51:56 +0100

    csv export adapted for Anki-App

--- src/utils/output_utils/create_csv_from_df.py
+++ src/utils/output_utils/create_csv_from_df.py
@@ -1,7 +1,5 @@
-def create_csv_from_df(df, filename="output/results.csv", limit=100):
-    if limit is None:
-        df = df[["token", "example phrase"]]
-    else:
-        df = df[["token", "example phrase"]].head(limit)
-
-    df.to_csv(filename)
+def create_csv_from_df(df, filename="output/results.csv", limit=50):
+    df = df[["token", "example phrase"]].head(limit)
+    df.columns = ["word", "example sentence"]
+    df = df.set_index("word")
+    df.to_csv(filename, header=False)
