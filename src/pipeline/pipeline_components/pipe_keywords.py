8f97b9962cc9e1e729f4c9b3f4544b48467fb55e
Joshua Kraft <nathan.joshua.kraft@gmx.de>
2021-02-24 19:00:40 +0100

    changed keyword parameter

--- src/pipeline/pipeline_components/pipe_keywords.py
+++ src/pipeline/pipeline_components/pipe_keywords.py
@@ -5,7 +5,7 @@ def check_keyphrases(doc):
     """extracting keywords out of the text and assigning each token
     a keyword score based on the simalarity"""
 
-    kw_extractor = KeywordExtractor(lan="en", n=1, top=100)
+    kw_extractor = KeywordExtractor(lan="en", n=1, top=500)
     kw = dict(kw_extractor.extract_keywords(doc.text))
 
     keywords_token = []
